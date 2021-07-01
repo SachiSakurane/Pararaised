de9905c9d3eb41c540a26e5e5bf21ef5644c2a5b
SachiSakurane <sakuranesachi@gmail.com>
2021-07-01 21:33:19 +0900

    initial commit

---
+++ Pararaised/README.md
@@ -0,0 +1,2 @@
+# Pararaised
+A basic volume control effect plug-in with IGraphics GUI
---
+++ Pararaised/config.h
@@ -0,0 +1,54 @@
+#define PLUG_NAME "Pararaised"
+#define PLUG_MFR "executeprojects"
+#define PLUG_VERSION_HEX 0x00000001
+#define PLUG_VERSION_STR "0.0.1"
+#define PLUG_UNIQUE_ID 'NvWr'
+#define PLUG_MFR_ID 'Acme'
+#define PLUG_URL_STR "https://twitter.com/sakurane_sachi"
+#define PLUG_EMAIL_STR "sakuranesachi@gmail.com"
+#define PLUG_COPYRIGHT_STR "Copyright 2021 execute projects"
+#define PLUG_CLASS_NAME Pararaised
+
+#define BUNDLE_NAME "Pararaised"
+#define BUNDLE_MFR "executeprojects"
+#define BUNDLE_DOMAIN "com"
+
+#define SHARED_RESOURCES_SUBPATH "Pararaised"
+
+#define PLUG_CHANNEL_IO "1-1 2-2"
+
+#define PLUG_LATENCY 0
+#define PLUG_TYPE 0
+#define PLUG_DOES_MIDI_IN 0
+#define PLUG_DOES_MIDI_OUT 0
+#define PLUG_DOES_MPE 0
+#define PLUG_DOES_STATE_CHUNKS 0
+#define PLUG_HAS_UI 1
+#define PLUG_WIDTH 600
+#define PLUG_HEIGHT 600
+#define PLUG_FPS 60
+#define PLUG_SHARED_RESOURCES 0
+#define PLUG_HOST_RESIZE 0
+
+#define AUV2_ENTRY Pararaised_Entry
+#define AUV2_ENTRY_STR "Pararaised_Entry"
+#define AUV2_FACTORY Pararaised_Factory
+#define AUV2_VIEW_CLASS Pararaised_View
+#define AUV2_VIEW_CLASS_STR "Pararaised_View"
+
+#define AAX_TYPE_IDS 'IEF1', 'IEF2'
+#define AAX_TYPE_IDS_AUDIOSUITE 'IEA1', 'IEA2'
+#define AAX_PLUG_MFR_STR "Acme"
+#define AAX_PLUG_NAME_STR "Pararaised\nIPEF"
+#define AAX_PLUG_CATEGORY_STR "Effect"
+#define AAX_DOES_AUDIOSUITE 1
+
+#define VST3_SUBCATEGORY "Fx"
+
+#define APP_NUM_CHANNELS 2
+#define APP_N_VECTOR_WAIT 0
+#define APP_MULT 1
+#define APP_COPY_AUV3 0
+#define APP_SIGNAL_VECTOR_SIZE 64
+
+#define ROBOTO_FN "Roboto-Regular.ttf"
