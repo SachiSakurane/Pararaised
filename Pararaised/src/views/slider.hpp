96f3ba609b143c88530ffedfb779175f44b6803e
SachiSakurane <sakuranesachi@gmail.com>
2021-07-12 00:59:02 +0900

    add slider

--- Pararaised/src/views/slider.hpp
+++ Pararaised/src/views/slider.hpp
@@ -3,12 +3,15 @@
 #include <iostream>
 
 #include <usagi/concepts.hpp>
+#include <usagi/geometry.hpp>
 #include <usagi/wrapper/icontrol/view_wrapper.hpp>
+#include <usagi/wrapper/skia/rect_wrapper.hpp>
 
 #include "SkPath.h"
 
 namespace views
 {
+  // knob ないやつは progress かも
   class slider final : public usagi::wrapper::icontrol::iplug_traits::base_view_type
   {
     using base_type = usagi::wrapper::icontrol::iplug_traits::base_view_type;
@@ -19,23 +22,29 @@ namespace views
 
     slider(const usagi::concepts::geometry::rect_concept auto &frame) : base_type{frame} {}
 
+    float normalized_value() { return 0.5f; }
+
     void draw(draw_context_type &context) override
     {
       context.draw(
           [this](auto &g)
           {
             auto f = frame();
-            SkPaint paint;
-            paint.setAntiAlias(true);
-            paint.setStyle(SkPaint::kStroke_Style);
-            paint.setStrokeWidth(2);
-            paint.setStrokeCap(SkPaint::kRound_Cap);
-            paint.setColor(SK_ColorCYAN);
-
+            auto [width, height] = f.size()();
+            SkPaint src, dst;
             SkCanvas *canvas = static_cast<SkCanvas *>(g.GetDrawContext());
-            SkPath path = SkPath::Line({f.l(), f.t()}, {f.r(), f.t()});
 
-            canvas->drawPath(path, paint);
+            canvas->clear(SK_ColorTRANSPARENT);
+            canvas->drawRRect(SkRRect::MakeRectXY(usagi::wrapper::skia::to_rect(f), height * 0.5f, height * 0.5f), dst);
+
+            // background
+            src.setBlendMode(SkBlendMode::kSrcIn);
+            src.setColor(SK_ColorLTGRAY);
+            canvas->drawPaint(src);
+
+            // colored
+            src.setColor(SK_ColorGRAY);
+            canvas->drawRect(usagi::wrapper::skia::to_rect(usagi::geometry::reduce_from_right(f, normalized_value() * width)), src);
           });
     }
 
